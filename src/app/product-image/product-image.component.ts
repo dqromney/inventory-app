957995a32b735f822567ba4ee725d26ebe7d0c4d
David Q. Romney <dromney@incomm.com>
2018-05-23 11:19:37 -0600

    Add product-image component.

---
+++ src/app/product-image/product-image.component.ts
@@ -0,0 +1,20 @@
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Product } from "../product.model";
+
+@Component({
+  selector: 'product-image',
+  templateUrl: `
+  <img class="product-image" [src]="product.imageUrl">
+  `
+})
+export class ProductImageComponent implements OnInit {
+
+  @Input() product: Product;
+  @HostBinding('attr.class') cssClass = 'ui small image';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
